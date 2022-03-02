a01a658c5ed308f0ebc8fb23dfc9306249d42dc0
Neha9849 <nehadeekonda66@gmail.com>
2022-03-02 21:02:38 +0530

    added callbackURl for deploy

--- passport.js
+++ passport.js
@@ -9,7 +9,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL: "https://root-blogging-app.herokuapp.com/auth/google/callback",
         scope: [ 'profile' ],
         state: true
       },
